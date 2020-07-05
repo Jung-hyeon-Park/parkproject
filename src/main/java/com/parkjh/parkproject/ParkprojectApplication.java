ecd90e286f79eb7021ce57591bf71429b06c1003
parkjh <tkarnrrns200@naver.com>
2020-07-05 20:04:05 +0900

    Initial commit (2020.07.05)

---
+++ src/main/java/com/parkjh/parkproject/ParkprojectApplication.java
@@ -0,0 +1,13 @@
+package com.parkjh.parkproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/parkjh/parkproject/ParkprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parkjh.parkproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
